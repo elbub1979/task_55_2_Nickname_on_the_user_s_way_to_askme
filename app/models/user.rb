734f520219d713e1b1d9bc46a85ff6b1c9bb24df
elbub1979 <ilya.leuh09@gmail.com>
2022-05-11 21:04:22 +0300

    ref routes.rb, ./model/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,18 @@
 class User < ApplicationRecord
   has_secure_password
 
-  def to_param
-    nickname
-  end
-
-
   before_save :downcase_nickname
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, :nickname, presence: true, uniqueness: true
 
   has_many :questions, dependent: :delete_all
 
   include Gravtastic
   gravtastic(secure: true, filetype: :png, size: 100, default: 'retro')
+
+  def to_param
+    nickname
+  end
 
   def downcase_nickname
     nickname.downcase!
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   
   resources :questions
   resource :session, only: %i[new create destroy]
-  resources :users, except: %i[index], key: :nickname
+  resources :users, except: %i[index], param: :nickname
 end
